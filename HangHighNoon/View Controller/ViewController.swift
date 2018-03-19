f9bb8b44c39d9244766f6b7b7fdc97a8dad82204
BrockTyler <35366483+BrockTyler@users.noreply.github.com>
2018-03-19 09:09:23 -0600

    Added auto constraints to storyboard items; updated view controller to display new word, score, and hang picks.

--- HangHighNoon/View Controller/ViewController.swift
+++ HangHighNoon/View Controller/ViewController.swift
@@ -24,7 +24,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     // Guessing Properties
     var wordCompleted: Bool = false
@@ -36,9 +35,9 @@ class ViewController: UIViewController {
     var guessedCharacters: [Character] = []
     
     // Image Properties
-    let hangImages: [UIImage] = [] // Add images in order of hanging sequence. Image displayed is determined by incorrectGuessCount.
-    let loserImage: UIImage = #imageLiteral(resourceName: <#T##String#>)
-    let winnerImage: UIImage = #imageLiteral(resourceName: <#T##String#>)
+    let hangImages: [UIImage] = [#imageLiteral(resourceName: "Hang0"), #imageLiteral(resourceName: "Hang1"), #imageLiteral(resourceName: "Hang2"), #imageLiteral(resourceName: "Hang3"), #imageLiteral(resourceName: "Hang4"), #imageLiteral(resourceName: "Hang5"), #imageLiteral(resourceName: "Hang6")] // Add images in order of hanging sequence. Image displayed is determined by incorrectGuessCount.
+    let loserImage: UIImage = #imageLiteral(resourceName: "Loser")
+    let winnerImage: UIImage = #imageLiteral(resourceName: "Winner")
     
     // Word Display Properties
     let block: String = "█"
@@ -47,18 +46,32 @@ class ViewController: UIViewController {
     var realWord: String = ""
     var formattedWord: String = ""
     
+    // Button properties
+    var buttonArray: [UIButton] = []
+    
     // Outlets
     @IBOutlet weak var hangImageView: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var wordDisplayLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
     }
-
+    
+    @IBAction func letterButtonTapped(_ sender: UIButton) {
+    
+        updateWordDisplayFormat(wordToFormat: formattedWord, selectedChar: Character((sender.titleLabel?.text)!))
+        sender.isSelected = true
+        sender.isEnabled = false
+        buttonArray.append(sender)
+    }
+    
     @IBAction func newWordButtonTapped(_ sender: Any) {
         
         newWord = WordBankController().selectNewWord()
         realWord = ""
+        formattedWord = ""
         wordCompleted = false
         userWon = false
         userLost = false
@@ -67,47 +80,82 @@ class ViewController: UIViewController {
         guessedCharacters.removeAll()
         updateWordDisplayFormat(wordToFormat: newWord, selectedChar: "*")
         updateHangImage(incorrectGuessCount: incorrectGuessCount)
+        
+        for button in buttonArray {
+            button.isEnabled = true
+            button.isSelected = false
+        }
+        
     }
 
     func updateWordDisplayFormat(wordToFormat: String, selectedChar: Character) -> Void {
         
         realWord = wordToFormat
         
+        // "*" used as selectedChar only if a brand new word was generated.
         if selectedChar != "*" {
             
             guessedCharacters.append(selectedChar)
             
             if !realWord.contains(guessedCharacters.last!) {
                 self.incorrectGuessCount += 1
-                guard self.incorrectGuessCount != maxIncorrectGuesses else { gameLost() }
+                if self.incorrectGuessCount == maxIncorrectGuesses {
+                    updateScore(incorrectGuessCount: incorrectGuessCount)
+                    updateHangImage(incorrectGuessCount: incorrectGuessCount)
+                    gameLost()
+                }
+                updateScore(incorrectGuessCount: incorrectGuessCount)
+                updateHangImage(incorrectGuessCount: incorrectGuessCount)
             } else {
-                for (index, char) in realWord.enumerated() {
+            
+                for char in Array(realWord) {
+                    
+                    // TODO: Set formattedWord char equal to realWord at indexes of guessedCharacters.last
+                    var count = 0
                     if char == guessedCharacters.last {
-                        
+                        let newFormattedWord = self.formattedWord.prefix(count) + "\(char)" + self.formattedWord.dropFirst(count + 1)
+                        self.formattedWord = "\(newFormattedWord)"
+                        count += 1
                     }
                 }
             }
+            
+            wordDisplayLabel.text = formattedWord
             // reveal guessed characters and increment guessCounts
             
             
         } else {
             
+            let endIndex = realWord.index(realWord.endIndex, offsetBy: 0)
+            
             // Spacing format for brand new word
-            for char in wordToFormat {
-                if char != wordToFormat.last {
+
+            for char in Array(realWord) {
+                if realWord.index(of: char) != endIndex {
                     realWord += "\(char) "
+                    realWord.removeFirst()
                     formattedWord += "\(block) "
                 } else {
                     realWord += "\(char)"
+                    realWord.removeFirst()
                     formattedWord += "\(block)"
                 }
             }
+            
+            String(realWord)
+            
+            wordDisplayLabel.text = formattedWord
+    
+            print(realWord)
+            print(formattedWord)
         }
         
         
     }
     
-    
+    func updateScore(incorrectGuessCount: Int) {
+        scoreLabel.text = "Score: \(incorrectGuessCount) / 7"
+    }
     
     func updateHangImage(incorrectGuessCount: Int) {
         let index = incorrectGuessCount
@@ -115,11 +163,11 @@ class ViewController: UIViewController {
     }
     
     func gameWon() {
-        
+        hangImageView.image = winnerImage
     }
     
     func gameLost() {
-        
+        hangImageView.image = loserImage
     }
     
 
